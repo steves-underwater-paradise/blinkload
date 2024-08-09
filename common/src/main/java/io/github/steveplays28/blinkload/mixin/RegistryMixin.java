7417b4ba44aab1dc15f76371bb5f6b0817ae8001
Steveplays28 <djspaargaren@outlook.com>
2024-08-09 15:08:15 +0200

    fix: Fix off-thread bootstrapping causing registry issues

--- common/src/main/java/io/github/steveplays28/blinkload/mixin/RegistryMixin.java
+++ common/src/main/java/io/github/steveplays28/blinkload/mixin/RegistryMixin.java
@@ -1,9 +1,8 @@
 package io.github.steveplays28.blinkload.mixin;
 
 import io.github.steveplays28.blinkload.client.event.ClientLifecycleEvent;
+import io.github.steveplays28.blinkload.util.ThreadUtil;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +11,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Registry.class)
 public interface RegistryMixin {
 	@Inject(method = "getOrThrow", at = @At(value = "HEAD"))
-	private void blinkload$waitForBootstrapInitializationToFinish(RegistryKey<?> key, @NotNull CallbackInfoReturnable<?> cir) {
+	private void blinkload$waitForBootstrapInitializationToFinishBeforeGetting(CallbackInfoReturnable<?> cir) {
+		if (Thread.currentThread().getName().contains(ThreadUtil.BOOTSTRAPPER_THREAD_POOL_NAME)) {
+			return;
+		}
+
+		ClientLifecycleEvent.BEFORE_BOOTSTRAP_FINISH_INITIALIZATION.invoker().onBeforeBootstrapFinishInitialization();
+	}
+
+	@Inject(method = {"register(Lnet/minecraft/registry/Registry;Lnet/minecraft/registry/RegistryKey;Ljava/lang/Object;)Ljava/lang/Object;", "register(Lnet/minecraft/registry/Registry;ILjava/lang/String;Ljava/lang/Object;)Ljava/lang/Object;", "registerReference(Lnet/minecraft/registry/Registry;Lnet/minecraft/registry/RegistryKey;Ljava/lang/Object;)Lnet/minecraft/registry/entry/RegistryEntry$Reference;"}, at = @At(value = "HEAD"))
+	private static void blinkload$waitForBootstrapInitializationToFinishBeforeRegistering(CallbackInfoReturnable<?> cir) {
+		if (Thread.currentThread().getName().contains(ThreadUtil.BOOTSTRAPPER_THREAD_POOL_NAME)) {
+			return;
+		}
+
 		ClientLifecycleEvent.BEFORE_BOOTSTRAP_FINISH_INITIALIZATION.invoker().onBeforeBootstrapFinishInitialization();
 	}
 }
