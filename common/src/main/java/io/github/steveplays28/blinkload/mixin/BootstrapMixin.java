6fec300694a7b155c5c16546914fdffaae108c2e
Steveplays28 <Djspaargaren@outlook.com>
2024-08-29 12:08:54 +0200

    Optimise crash report, GLX and bootstrap initializations (#2)

---
+++ common/src/main/java/io/github/steveplays28/blinkload/mixin/BootstrapMixin.java
@@ -0,0 +1,33 @@
+package io.github.steveplays28.blinkload.mixin;
+
+import io.github.steveplays28.blinkload.event.LifecycleEvent;
+import io.github.steveplays28.blinkload.util.ThreadUtil;
+import net.minecraft.Bootstrap;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Bootstrap.class)
+public class BootstrapMixin {
+	@Unique
+	private static boolean blinkload$hasFinished = false;
+
+	@Inject(method = "ensureBootstrapped", at = @At(value = "HEAD"))
+	private static void blinkload$finishBootstrappingIfRequired(@NotNull CallbackInfo ci) {
+		if (blinkload$hasFinished || Thread.currentThread().getName().contains(ThreadUtil.BOOTSTRAPPER_THREAD_POOL_NAME)) {
+			return;
+		}
+
+		LifecycleEvent.BEFORE_BOOTSTRAP_FINISH_INITIALIZATION.invoker().onBeforeBootstrapFinishInitialization();
+		blinkload$hasFinished = true;
+	}
+
+	@Redirect(method = "initialize", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemGroups;collect()V"))
+	private static void blinkload$cancelItemGroupsCollect() {
+		// NO-OP
+	}
+}
